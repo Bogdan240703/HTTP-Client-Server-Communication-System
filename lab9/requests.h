86b97c69401bf85aa2cd969656d2a3d2bf9f30cf
Bogdan240703 <120594126+Bogdan240703@users.noreply.github.com>
2025-02-28 01:17:30 +0200

    Delete lab9 directory

--- lab9/requests.h
+++
@@ -1,8 +0,0 @@
-#ifndef _REQUESTS_
-#define _REQUESTS_
-
-char *creaza_mesag_get(char *host, char *url, char *cookie, char *token, int e_mesaj_tip_get);
-
-char *creaza_mesaj_post(char *host, char *url, char *tip_payload, char *payload, char *cookie, int trebuie_autorizatie);
-
-#endif
